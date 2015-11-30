64691206ef30b8207b5b7389f99990a06cee7f8e
ghrapan <gena@entrypointsoft.com>
2015-11-30 14:45:23 +0200

    Adding constructors to IdentityDbContext.

--- src/AspNet.Identity.EntityFramework6/IdentityDbContext.cs
+++ src/AspNet.Identity.EntityFramework6/IdentityDbContext.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,7 +15,66 @@ namespace AspNet.Identity.EntityFramework6
     /// Base class for the Entity Framework database context used for identity.
     /// </summary>
     public class IdentityDbContext : IdentityDbContext<IdentityUser> {
-        public IdentityDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection string for the database to which a connection will be made.
+        /// </summary>
+        public IdentityDbContext(string connectionString) : base(connectionString) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect to a database, and initializes it from
+        /// the given model.  The connection will not be disposed when the context is disposed if contextOwnsConnection is
+        /// false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="model">The model that will back this context.</param>
+        /// <param name="contextOwnsConnection">Constructs a new context instance using the existing connection to connect to a
+        /// database, and initializes it from the given model.  The connection will not be disposed when the context is
+        /// disposed if contextOwnsConnection is false.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
+            : base(existingConnection, model, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using conventions to create the name of
+        /// the database to which a connection will be made, and initializes it from
+        /// the given model.  The by-convention name is the full name (namespace + class
+        /// name) of the derived context class.  See the class remarks for how this is
+        /// used to create a connection.
+        /// </summary>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(DbCompiledModel model)
+            : base(model)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect
+        /// to a database.  The connection will not be disposed when the context is disposed
+        /// if contextOwnsConnection is false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="contextOwnsConnection">If set to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection
+        /// string for the database to which a connection will be made, and initializes
+        /// it from the given model.  See the class remarks for how this is used to create
+        /// a connection.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(string connectionString, DbCompiledModel model)
+            : base(connectionString, model)
+        {
+        }
     }
 
     /// <summary>
@@ -24,7 +84,66 @@ namespace AspNet.Identity.EntityFramework6
     public class IdentityDbContext<TUser> : IdentityDbContext<TUser, IdentityRole, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim, IdentityRoleClaim>
         where TUser : IdentityUser
     {
-        public IdentityDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection string for the database to which a connection will be made.
+        /// </summary>
+        public IdentityDbContext(string connectionString) : base(connectionString) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect to a database, and initializes it from
+        /// the given model.  The connection will not be disposed when the context is disposed if contextOwnsConnection is
+        /// false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="model">The model that will back this context.</param>
+        /// <param name="contextOwnsConnection">Constructs a new context instance using the existing connection to connect to a
+        /// database, and initializes it from the given model.  The connection will not be disposed when the context is
+        /// disposed if contextOwnsConnection is false.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
+            : base(existingConnection, model, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using conventions to create the name of
+        /// the database to which a connection will be made, and initializes it from
+        /// the given model.  The by-convention name is the full name (namespace + class
+        /// name) of the derived context class.  See the class remarks for how this is
+        /// used to create a connection.
+        /// </summary>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(DbCompiledModel model)
+            : base(model)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect
+        /// to a database.  The connection will not be disposed when the context is disposed
+        /// if contextOwnsConnection is false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="contextOwnsConnection">If set to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection
+        /// string for the database to which a connection will be made, and initializes
+        /// it from the given model.  See the class remarks for how this is used to create
+        /// a connection.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(string connectionString, DbCompiledModel model)
+            : base(connectionString, model)
+        {
+        }
     }
 
     /// <summary>
@@ -45,7 +164,66 @@ namespace AspNet.Identity.EntityFramework6
         where TUserClaim : IdentityUserClaim<TKey>
         where TRoleClaim : IdentityRoleClaim<TKey>
     {
-        public IdentityDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection string for the database to which a connection will be made.
+        /// </summary>
+        public IdentityDbContext(string connectionString) : base(connectionString) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect to a database, and initializes it from
+        /// the given model.  The connection will not be disposed when the context is disposed if contextOwnsConnection is
+        /// false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="model">The model that will back this context.</param>
+        /// <param name="contextOwnsConnection">Constructs a new context instance using the existing connection to connect to a
+        /// database, and initializes it from the given model.  The connection will not be disposed when the context is
+        /// disposed if contextOwnsConnection is false.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
+            : base(existingConnection, model, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using conventions to create the name of
+        /// the database to which a connection will be made, and initializes it from
+        /// the given model.  The by-convention name is the full name (namespace + class
+        /// name) of the derived context class.  See the class remarks for how this is
+        /// used to create a connection.
+        /// </summary>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(DbCompiledModel model)
+            : base(model)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the existing connection to connect
+        /// to a database.  The connection will not be disposed when the context is disposed
+        /// if contextOwnsConnection is false.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="contextOwnsConnection">If set to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </param>
+        public IdentityDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext" /> class using the connection
+        /// string for the database to which a connection will be made, and initializes
+        /// it from the given model.  See the class remarks for how this is used to create
+        /// a connection.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <param name="model">The model that will back this context.</param>
+        public IdentityDbContext(string connectionString, DbCompiledModel model)
+            : base(connectionString, model)
+        {
+        }
 
         /// <summary>
         /// Gets or sets the <see cref="DbSet{TEntity}"/> of Users.
