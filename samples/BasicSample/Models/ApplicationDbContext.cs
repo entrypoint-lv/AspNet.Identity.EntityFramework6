ec76d21889ea9ef984abdaf06cbcf72817cd6810
ghrapan <gena@entrypointsoft.com>
2016-01-21 17:55:24 +0200

    Adding basic sample.

---
+++ samples/BasicSample/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+using AspNet.Identity.EntityFramework6;
+
+namespace BasicSample.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+    }
+}
